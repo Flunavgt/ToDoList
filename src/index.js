e0d971700a50efc47a0887540e3a786c4fe0255f
Federico <fluna1973@yahoo.com>
2022-08-09 18:56:44 -0600

    correct indents

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import _ from 'lodash';
+// import _ from 'lodash';
 import './style.css';
 
 const arr1 = [
@@ -16,28 +16,12 @@ const arr1 = [
   },
   {
     id: 3,
-    description: 'Merge branch',
+    description: 'Merge ',
   },
 ];
 
-
-  document.querySelector('.ToDolistGen').innerHTML = arr1.map((items) => `<div class="completeList">
+document.querySelector('.ToDolistGen').innerHTML = arr1.map((items) => `<div class="completeList">
   <div class="toDo">
   <input type="checkbox" id="checkB">
   <p class="workdescription" id="firstcard">${items.description}</p> 
   </div>`).join('');
-
-  
-
-
-// function component() {
-//   const element = document.createElement('div');
-
-//   // Lodash, now imported by this script
-//   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-//   element.classList.add('hello');
-
-//   return element;
-// }
-
-// document.body.appendChild(component());
